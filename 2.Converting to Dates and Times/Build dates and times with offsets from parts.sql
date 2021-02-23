5ea4644c52e6b59e2c404cf1f5993787ebe5c0bb
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-23 13:34:37 +0700

    Update Build dates and times with offsets from parts.sql

--- 2.Converting to Dates and Times/Build dates and times with offsets from parts.sql
+++ 2.Converting to Dates and Times/Build dates and times with offsets from parts.sql
@@ -1,5 +1,5 @@
 SELECT
-	-- Fill in the millisecond PRIOR TO chaos
+    -- Fill in the millisecond PRIOR TO chaos
 	DATETIMEOFFSETFROMPARTS(2038, 01, 19, 3, 14, 07, 999, 0, 0, 3) AS LastMoment,
     -- Fill in the date and time when we will experience the Y2.038K problem
     -- Then convert to the Eastern Standard Time time zone
